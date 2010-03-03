395028d31d5eb24facc9177fd23e4bb0307f8f46
dan marks <danmarks@dan-markss-imac.local>
2010-03-03 10:02:09 -0800

    changed count to size in model validation also...

--- app/models/location.rb
+++ app/models/location.rb
@@ -15,7 +15,7 @@ class Location < ActiveRecord::Base
     geo=Geokit::Geocoders::MultiGeocoder.geocode(self.full_address)
     
     if geo.success
-      if geo.all.count == 1
+      if geo.all.size == 1
         self.full_address = geo.full_address
         self.street_address, self.city, self.state, self.zip = geo.street_address, geo.city, geo.state, geo.zip
         self.country_code, self.province = geo.country_code, geo.province
